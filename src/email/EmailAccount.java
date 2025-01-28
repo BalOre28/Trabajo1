5af5f17ba0d85a4d2ced64339e98dce4d659e1f8
Balto <Balto@DESKTOP-BN4JQTL>
2025-01-28 14:09:38 -0600

    terminando las funciones de la Clase EmailAccount

--- src/email/EmailAccount.java
+++ src/email/EmailAccount.java
@@ -43,5 +43,31 @@ public class EmailAccount {
         inbox.add(email);
     }
 
-   
+    public void printInbox() {
+        System.out.println("Correo: " + correo);
+        System.out.println("Usuario: " + usuario);
+        for (int i = 0; i < inbox.size(); i++) {
+            Email email = inbox.get(i);
+            if (email != null) {
+                System.out.println("Posición " + (i + 1) + "\n\tEl Correo Emisor pertenece a: "
+                        + email.getreciEmail() + "n\tAsunto: " + email.getAsunto()
+                        + "\tEmail Leídos/Sin Leer: " + email.isLeido());
+            }
+        }
+    }
+
+    public void leerEmail(int posicion) {
+        posicion--;
+        if (posicion >= 0 && posicion < inbox.size() && inbox.get(posicion) != null) {
+            inbox.get(posicion).print();
+            inbox.get(posicion).setLeido();
+        } else {
+            System.out.println("El correo no existe");
+        }
+    }
+
+    public void borrarLeidos() {
+        inbox.removeIf(Email::isLeido);
+    }
+
 }
